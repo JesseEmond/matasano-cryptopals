0082b67179ac3cf17ec2d346a22670fdbaddb81e
JesseEmond <emond.jesse@gmail.com>
2016-04-19 02:55:47 -0400

    Set 1 challenge 6

---
+++ set1/6.py
@@ -0,0 +1,44 @@
+from dist import hamming
+from base64 import b64decode
+from xor import xor_single_char_key, xor_repeating_key
+from freq import english_test
+
+
+def guess_keysize(cipher):
+    def score(cipher, size):
+        distances = [hamming(cipher[i * size:(i+1) * size],
+                             cipher[(i+1) * size:(i+2) * size])
+                     for i in range(len(cipher) // size - 2)]
+        return sum(distances) / len(distances) / size
+
+    return min(range(2, 41), key=lambda size: score(cipher, size))
+
+
+def brute(cipher):
+    size = guess_keysize(cipher)
+    print("guessed keysize %i" % size)
+
+    blocks = [cipher[i * size:(i+1) * size]
+              for i in range(len(cipher) // size)]
+    nth_bytes = zip(*blocks)
+    key = [break_single_char(bytes_) for bytes_ in nth_bytes]
+
+    return bytes(key)
+
+
+def break_single_char(cipher):
+    keyspace = range(256)
+    decrypted = [(key, xor_single_char_key(cipher, key)) for key in keyspace]
+    best = max(decrypted, key=lambda pair: english_test(pair[1]))
+    return best[0]
+
+
+with open("6.txt") as f:
+    lines = f.readlines()
+
+cipher = b64decode(''.join(lines))
+key = brute(cipher)
+print("key : %s" % key.decode('ascii'))
+
+decrypted = xor_repeating_key(cipher, key)
+print(decrypted.decode('ascii'))
