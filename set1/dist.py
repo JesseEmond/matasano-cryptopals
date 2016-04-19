349c5cf5ca4f1277821c6915d74200e9442bb494
JesseEmond <emond.jesse@gmail.com>
2016-04-19 01:24:34 -0400

    Hamming distance

---
+++ set1/dist.py
@@ -0,0 +1,15 @@
+def hamming(a, b):
+    assert(len(a) == len(b))
+
+    dist = 0
+    for x, y in zip(a, b):
+        z = x ^ y
+        while z:
+            dist += z & 1
+            z >>= 1
+
+    return dist
+
+
+assert(hamming("this is a test".encode('ascii'),
+               "wokka wokka!!!".encode('ascii')) == 37)
