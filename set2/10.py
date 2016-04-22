f76ef033efa9349c0cf09c8a67935d16971c8b6e
JesseEmond <emond.jesse@gmail.com>
2016-04-22 12:31:12 -0400

    Refactor AES logic to its own file and fix ignores

--- set2/10.py
+++ set2/10.py
@@ -1,47 +1,5 @@
-from Crypto.Cipher import AES
+from aes import ecb_encrypt, ecb_decrypt, cbc_encrypt, cbc_decrypt
 from base64 import b64decode
-
-
-def ecb_encrypt(key, block):
-    suite = AES.new(key, AES.MODE_ECB)
-    return suite.encrypt(block)
-
-
-def ecb_decrypt(key, block):
-    suite = AES.new(key, AES.MODE_ECB)
-    return suite.decrypt(block)
-
-
-def get_blocks(bytes_):
-    return [bytes_[i:i+16] for i in range(0, len(bytes_), 16)]
-
-
-def xor_blocks(a, b):
-    return bytes([x ^ y for x, y in zip(a, b)])
-
-
-def cbc_encrypt(key, iv, plaintext):
-    blocks = get_blocks(plaintext)
-    ciphertext = bytearray()
-
-    for block in blocks:
-        encrypted = ecb_encrypt(key, xor_blocks(iv, block))
-        ciphertext.extend(encrypted)
-        iv = encrypted
-
-    return ciphertext
-
-
-def cbc_decrypt(key, iv, ciphertext):
-    blocks = get_blocks(ciphertext)
-    plaintext = bytearray()
-
-    for block in blocks:
-        decrypted = ecb_decrypt(key, block)
-        plaintext.extend(xor_blocks(iv, decrypted))
-        iv = block
-
-    return plaintext
 
 
 key = "YELLOW SUBMARINE"
