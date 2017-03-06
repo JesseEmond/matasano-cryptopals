bd03aedf3af940a8178d170a6333c43a11876a08
JesseEmond <emond.jesse@gmail.com>
2017-03-06 16:12:36 -0500

    Refactor break_xor_repeating_key

--- src/06.py
+++ src/06.py
@@ -1,37 +1,20 @@
 from distance import hamming
 from base64 import b64decode
-from xor import xor_single_char_key, xor_repeating_key, break_xor_char_key
+from xor import (xor_single_char_key, xor_repeating_key,
+                 break_xor_repeating_key, guess_key_lengths)
 from frequency import english_test
-
-
-def guess_keysize(cipher):
-    def score(cipher, size):
-        distances = [hamming(cipher[i * size:(i+1) * size],
-                             cipher[(i+1) * size:(i+2) * size])
-                     for i in range(len(cipher) // size - 2)]
-        return sum(distances) / len(distances) / size
-
-    return min(range(2, 41), key=lambda size: score(cipher, size))
-
-
-def brute(cipher):
-    size = guess_keysize(cipher)
-    print("guessed keysize %i" % size)
-
-    blocks = [cipher[i * size:(i+1) * size]
-              for i in range(len(cipher) // size)]
-    nth_bytes = zip(*blocks)
-    key = [break_xor_char_key(bytes_) for bytes_ in nth_bytes]
-
-    return bytes(key)
 
 
 with open("06.txt") as f:
     lines = f.readlines()
 
 cipher = b64decode(''.join(lines))
-key = brute(cipher)
-print("key : %s" % key.decode('ascii'))
+
+key_length = guess_key_lengths(cipher)[0]
+print("guessed key length: %i" % key_length)
+
+key = break_xor_repeating_key(cipher, key_length)
+print("key: %s" % key.decode('ascii'))
 
 decrypted = xor_repeating_key(cipher, key)
 message = decrypted.decode('ascii')
--- src/xor.py
+++ src/xor.py
@@ -1,4 +1,5 @@
 from frequency import english_test
+from distance import hamming
 
 
 def xor_bytes(a, b):
@@ -13,9 +14,33 @@ def xor_repeating_key(msg, key):
     repeats, leftover = divmod(len(msg), len(key))
     return xor_bytes(msg, bytes(key * repeats + key[:leftover]))
 
+
 def break_xor_char_key(ciphertext, quality_test=english_test):
     possible_keys = range(256)
     best_key = max(possible_keys,
                    key=lambda k:
                    quality_test(xor_single_char_key(ciphertext, k)))
     return best_key
+
+
+def break_xor_repeating_key(ciphertext, key_length,
+                            quality_test=english_test):
+    blocks = [ciphertext[i * key_length:(i+1) * key_length]
+              for i in range(len(ciphertext) // key_length)]
+    nth_bytes = zip(*blocks)
+    key = bytes([break_xor_char_key(bytes_, quality_test)
+                 for bytes_ in nth_bytes])
+
+    return key
+
+
+def guess_key_lengths(cipher, min_length=2, max_length=40,
+                      distance_fn=hamming):
+    def score(cipher, size):
+        distances = [distance_fn(cipher[i * size:(i+1) * size],
+                                 cipher[(i+1) * size:(i+2) * size])
+                     for i in range(len(cipher) // size - 2)]
+        return sum(distances) / len(distances) / size
+
+    return sorted(range(min_length, max_length+1),
+                  key=lambda size: score(cipher, size))
