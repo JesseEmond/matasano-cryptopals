3d320bc4a7fab90aca094aa9db11c22287e7362e
JesseEmond <emond.jesse@gmail.com>
2017-03-06 18:55:05 -0500

    Challenge 20

--- src/20.py
+++ src/20.py
@@ -2,6 +2,7 @@ from base64 import b64decode
 from os import urandom
 
 from aes import ctr_encrypt
+from xor import xor_bytes, rank_xor_repeating_keys
 
 
 key = urandom(16)
@@ -17,4 +18,17 @@ secrets = [ctr_encrypt(key, nonce, secret) for secret in secrets]
 min_length = min(len(secret) for secret in secrets)
 truncated = [secret[:min_length] for secret in secrets]
 
-concatenated = "".join(truncated)
+concatenated = b"".join(truncated)
+keys = rank_xor_repeating_keys(concatenated, min_length)
+
+# found through analysis
+key_indices = {
+        0: 3,
+        }
+key = bytes([keys[i][key_indices.get(i, 0)] for i in range(len(keys))])
+
+plaintexts = [xor_bytes(secret, key) for secret in secrets]
+
+for i in range(len(expected)):
+    print(plaintexts[i])
+    assert(expected[i].startswith(plaintexts[i]))
