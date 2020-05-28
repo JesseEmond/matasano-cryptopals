1f7b22883248caaf1c306bb95999fb1d31d8d473
Jesse Emond <emond.jesse@gmail.com>
2020-05-28 12:04:07 -0400

    Add HMAC-SHA1 implementation

--- src/mac.py
+++ src/mac.py
@@ -1,14 +1,33 @@
-from md4 import md4
-from sha1 import sha1
+import md4
+import sha1
+import xor
 
 
-def keyed_mac(key, message, hash_):
-    return hash_(key + message)
+def keyed_mac(key, message, hash_fn):
+    return hash_fn(key + message)
+
+
+def hmac(key, message, hash_class):
+    if len(key) < hash_class.BLOCK_SIZE:
+        key = key.ljust(hash_class.BLOCK_SIZE, b"\x00")
+    elif len(key) > hash_class.BLOCK_SIZE:
+        key = hash_class().update(key).digest()
+    outer_key = xor.xor_single_char_key(key, 0x5c)
+    inner_key = xor.xor_single_char_key(key, 0x36)
+    hash_fn = lambda data: hash_class().update(data).digest()
+    inner = keyed_mac(inner_key, message, hash_fn)
+    return keyed_mac(outer_key, inner, hash_fn)
 
 
 def sha1_keyed_mac(key, message):
-    return keyed_mac(key, message, sha1)
+    return keyed_mac(key, message, sha1.sha1)
 
 
 def md4_keyed_mac(key, message):
-    return keyed_mac(key, message, md4)
+    return keyed_mac(key, message, md4.md4)
+
+
+def hmac_sha1(key, message):
+    return hmac(key, message, sha1.Sha1)
+assert(hmac_sha1(b"key", b"The quick brown fox jumps over the lazy dog") ==
+    bytes.fromhex("de7c9b85b8b78aa6bc8a7a36f70a90701c9db4d9"))
